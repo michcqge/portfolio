73d144ef516fc0fe82c0609e9ffc79f90abb1b4b
michcqge <michaelcqge@gmail.com>
2024-08-26 23:52:34 -0400

    About me description and image

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="px-20">
         <main className="grid">
           <section className="bg-ocean min-h-screen">
-            <div className="text-xl antialiased font-mono text-michael pl-20 pt-20 ease-in duration-700 transition-opacity opacity-100 hover:opacity-10">
+            <div className="text-xl antialiased font-mono text-michael pl-20 pt-20 ease-in duration-700 transition-opacity opacity-30 hover:opacity-100">
               <p>👋 Hi, my name is </p>
               <p className="text-michael-white text text-5xl mt-4 text-responsive-heading">
                 Michael Ge.
@@ -35,17 +35,39 @@ export default function Home() {
               </p>
             </div>
           </section>
-          <section
-            id="about"
-            className="bg-ocean min-h-screen flex items-center justify-center"
-          >
-            <h1>About Me</h1>
-            <Image
-              src="MichaelAvatar.svg"
-              alt="That's me!"
-              width={300}
-              height={300}
-            />
+          <section id="about" className="bg-ocean px-60 py-10 m-auto">
+            <h1 className="text-michael-white text text-4xl mt-2 text-responsive-heading">
+              📝About me
+            </h1>
+            <section
+              id="aboutme"
+              className="grid xl:grid-cols-3 grid-cols gap-10"
+            >
+              <div className="col-span-2 pt-8 min-w-60 text-sm space-y-4 pl-4 text-gray-300">
+                <p>
+                  Hi! My name is Michael. I work as a full-stack developer at
+                  UKG. I like to experiment with AI tools and develop web
+                  technologies. I mainly code in Java, Typescript and
+                  PostgreSQL, but I am always learning about emerging tech.
+                </p>
+                <p>
+                  Some tools I use to deliver headlines include Docker,
+                  Kubernetes, Jenkins CI/CD, Copilot, Git and Github.
+                </p>
+                <p>
+                  I strive to foster a friendly and inclusive environment where
+                  everyone feels comfortable contributing. Because of this,
+                  people describe me as overly patient and understanding.{" "}
+                </p>
+              </div>
+              <Image
+                src="MichaelAvatar.svg"
+                alt="That's me!"
+                width={300}
+                height={300}
+                className="min-w-40 max-w-40 relative top-[-70px]"
+              />
+            </section>
             {/* <Room /> */}
           </section>
         </main>
@@ -55,6 +77,7 @@ export default function Home() {
             className="bg-ocean min-h-screen flex items-center justify-center"
           >
             <h1>Experience</h1>
+
             <div>
               <p>Get in touch</p>
             </div>
